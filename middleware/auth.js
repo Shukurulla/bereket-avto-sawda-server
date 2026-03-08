42e05e3389987414b32b01455bd8210cb3c862d6
Shukurulla <shukurullatursinbayev978@gmail.com>
2026-03-08 20:49:48 +0500

    start

--- middleware/auth.js
+++ middleware/auth.js
@@ -19,6 +19,14 @@ exports.protect = async (req, res, next) => {
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET, { ignoreExpiration: true });
     req.user = await User.findById(decoded.id);
+
+    if (!req.user) {
+      return res.status(401).json({
+        success: false,
+        message: 'Foydalanuvchi topilmadi'
+      });
+    }
+
     next();
   } catch (error) {
     return res.status(401).json({
